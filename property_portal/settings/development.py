d3f7e593a5839afc527255b3f852a1269887bef4
Om5999 <118242395+Om5999@users.noreply.github.com>
2024-07-15 04:15:53 +0530

    settings(base).py,development,production

---
+++ property_portal/settings/development.py
@@ -0,0 +1,16 @@
+from .base import *
+from . import env
+
+# Now you can use env safely in your current module
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": env("POSTGRES_ENGINE"),
+        "NAME": env("POSTGRES_DB"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("PG_HOST"),
+        "PORT": env("PG_PORT"),
+    }
+}
